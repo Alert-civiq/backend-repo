479a6b5ef9b04078a8b39699aab17ca2d2bdc42a
GGX <djimiluna@gmail.com>
2026-06-23 02:03:30 +0100

    Setup basique de l'API

---
+++ app.js
@@ -0,0 +1,37 @@
+//Setup basique du serveur Express
+const port = 3000;
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+//Définition de la clé secrète pour le JWT
+process.env.JWT_KEY = process.env.JWT_KEY || 'Cle_random_pour_le_jwt';
+
+//Setup de la connexion à la base de données MySQL
+const mysql = require('mysql2');
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'Alert-Civiq'
+});
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+
+app
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
